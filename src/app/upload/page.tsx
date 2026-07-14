8bea5ff89efa8823cb5cefccf7d5cb35da0f6389
Fabrice Werger <fabricewerger@icloud.com>
2026-07-14 12:10:19 +0200

    feat: add .fit file guide to upload page and align stats table

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -10,6 +10,21 @@ export default async function Upload() {
         <>
             <h1 className={styles.heading}>upload fit file</h1>
             <UploadForm gear={await listGear(await currentUserId())} />
+            <details className={styles.howto}>
+                <summary>how do I get a .fit file?</summary>
+                <ol>
+                    <li>
+                        open{' '}
+                        <a href="https://connect.garmin.com" target="_blank" rel="noreferrer">
+                            connect.garmin.com
+                        </a>{' '}
+                        on desktop (not the app)
+                    </li>
+                    <li>go to the activity's detail page</li>
+                    <li>click the share icon (top right) and choose "export file"</li>
+                    <li>upload the downloaded .fit file here</li>
+                </ol>
+            </details>
         </>
     );
 }
